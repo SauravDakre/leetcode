1667fcb98d5b38b72429d850af57d8a632ae73b6
saurav dakre <saurav.dakre@gmail.com>
2021-05-20 23:52:40 +0530

    least common ancestor

---
+++ binaryTree/lca/lca.go
@@ -0,0 +1,26 @@
+package lca
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	l := lowestCommonAncestor(root.Left, p, q)
+	r := lowestCommonAncestor(root.Right, p, q)
+	if l != nil && r != nil {
+		return root
+	} else if l != nil {
+		return l
+	} else {
+		return r
+	}
+}
---
+++ bst/LCA/lca.go
@@ -0,0 +1,20 @@
+package LCA
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else {
+		return root
+	}
+}
