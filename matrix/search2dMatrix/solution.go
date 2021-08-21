94d94531570f72e41cf668f119e9d71a292f44ad
saurav dakre <saurav.dakre@gmail.com>
2021-08-21 12:22:36 +0530

    search element in 2d sorted matrix using binary serach

--- matrix/search2dMatrix/solution.go
+++ matrix/search2dMatrix/solution.go
@@ -15,3 +15,39 @@ func searchMatrix(matrix [][]int, target int) bool {
 	}
 	return false
 }
+
+// using binary search
+func searchMatrixViaBinarySearch(matrix [][]int, target int) bool {
+	s := 0
+	e := len(matrix) - 1
+	for s <= e {
+
+		mid := (s + e) / 2
+		if target <= matrix[mid][len(matrix[0])-1] && target >= matrix[mid][0] {
+			return binSearch1D(matrix[mid], target)
+		}
+		if target > matrix[mid][0] {
+			s = mid + 1
+		} else {
+			e = mid - 1
+		}
+	}
+	return false
+}
+
+func binSearch1D(arr []int, k int) bool {
+	l := 0
+	r := len(arr) - 1
+	for l <= r {
+		mid := (l + r) / 2
+		if arr[mid] == k {
+			return true
+		}
+		if k > arr[mid] {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return false
+}
